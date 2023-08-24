9cb2d9fc885ebaf3814f31acae98df9bd20a366d
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-24 09:23:24 -0700

    Nop Function

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - typically nope
+ * @s: pointer to the pointer pointing to head of stack
+ * @iloc: integer representing the line number
+ */
+void nop(stack_t *s, unsigned int iloc)
+{
+	(void)s;
+	(void)iloc;
+}
